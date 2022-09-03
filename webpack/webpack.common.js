2fca5a0a2ae25b6e0301e9a1a32b861bf947d10b
章翀 <zhangchong.123@bytedance.com>
2022-09-03 22:33:46 +0800

    update dependencies
    Change-Id: Ie42e67ba19fdcb6751045bb3a168565c471aa606

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,39 +5,45 @@ const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
     entry: {
-      popup: path.join(srcDir, 'popup.tsx'),
-      options: path.join(srcDir, 'options.tsx'),
-      background: path.join(srcDir, 'background.ts'),
-      content_script: path.join(srcDir, 'content_script.tsx'),
+        popup: path.join(srcDir, 'popup.tsx'),
+        options: path.join(srcDir, 'options.tsx'),
+        background: path.join(srcDir, 'background.ts'),
+        content_script: path.join(srcDir, 'content_script.tsx'),
     },
     output: {
         path: path.join(__dirname, "../dist/js"),
         filename: "[name].js",
     },
+    // pack together
     optimization: {
         splitChunks: {
             name: "vendor",
             chunks(chunk) {
-              return chunk.name !== 'background';
+                return chunk.name !== 'background';
             }
         },
     },
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+                options: {}
             },
-        ],
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
+    performance: {
+        hints: false
     },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
-    ],
 };
